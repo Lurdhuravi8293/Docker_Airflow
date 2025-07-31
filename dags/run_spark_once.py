3a4e9e3566ff19cb7e67d30702f6c8a16ae65e86
=Lurdhuravi <=lurdhuravi123@gmail.com>
2025-07-31 10:28:38 -0700

    final code

--- dags/run_spark_once.py
+++ dags/run_spark_once.py
@@ -1,3 +1,4 @@
+
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
@@ -10,12 +11,17 @@ with DAG(
     dag_id='run_spark_once',
     schedule_interval=None,
     default_args=default_args,
+    start_date=datetime(2025, 7, 30),
     catchup=False,
     description='Run Spark job once',
+    tags=['spark', 'etl'],
 ) as dag:
 
     run_spark_job = SparkSubmitOperator(
         task_id='spark_submit_etl',
         application='/opt/etl/etl_pipeline.py',
+        master='spark://spark:7077', 
         conn_id='spark_default',
+        verbose=True,
+        spark_binary='spark-submit'
     )
